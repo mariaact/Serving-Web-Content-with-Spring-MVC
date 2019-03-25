d23d9bffc79938f144d2eba1dcbcf975a7484080
mcalvot <Esma2019*>
2019-03-25 08:52:24 +0100

    first commit

---
+++ src/main/java/org/mcalvot/formacion/ServingWebContentWithSpringMVC/ServingWebContentWithSpringMvcApplication.java
@@ -0,0 +1,13 @@
+package org.mcalvot.formacion.ServingWebContentWithSpringMVC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServingWebContentWithSpringMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServingWebContentWithSpringMvcApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mcalvot/formacion/ServingWebContentWithSpringMVC/ServingWebContentWithSpringMvcApplicationTests.java
@@ -0,0 +1,16 @@
+package org.mcalvot.formacion.ServingWebContentWithSpringMVC;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ServingWebContentWithSpringMvcApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
